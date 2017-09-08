16557399f30cdd3891624210ae8d200463b3fc80
RINOF <RINOF@DESKTOP-GPU3UFK>
2017-09-09 02:23:59 +0300

    1112

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             System.Console.WriteLine("first");
+            System.Console.WriteLine("first");
         }
     }
 }
